33d737b42a3adbbd6767e6aad8727336c02657d9
David-Shi-1989 <287841605@qq.com>
2018-09-14 16:59:28 +0800

    相册卡片提出作为组件

---
+++ src/components/album/album-card.vue
@@ -0,0 +1,182 @@
+<template>
+  <div>
+    <router-link class="sww-album-title" to="/album/123">{{title}}</router-link>
+    <p class="sww-album-count"><span>{{count}}</span>张</p>
+    <table :class="['sww-album-tb','sww-album-tb-'+disModeClass]">
+      <tbody v-if="disModeClass=='1'">
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[0]+'\')'}"></div></td>
+        </tr>
+      </tbody>
+      <tbody v-if="disModeClass=='2-h'">
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[0]+'\')'}"></div></td>
+        </tr>
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[1]+'\')'}"></div></td>
+        </tr>
+      </tbody>
+      <tbody v-if="disModeClass=='2-v'">
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[0]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[1]+'\')'}"></div></td>
+        </tr>
+      </tbody>
+      <tbody v-if="disModeClass=='3'">
+        <tr>
+          <td rowspan="2"><div :style="{backgroundImage:'url(\''+imgArr[0]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[1]+'\')'}"></div></td>
+        </tr>
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[2]+'\')'}"></div></td>
+        </tr>
+      </tbody>
+      <tbody v-if="disModeClass=='4'">
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[0]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[1]+'\')'}"></div></td>
+        </tr>
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[2]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[3]+'\')'}"></div></td>
+        </tr>
+      </tbody>
+      <tbody v-if="disModeClass=='5'">
+        <tr>
+          <td rowspan="2"><div :style="{backgroundImage:'url(\''+imgArr[0]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[1]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[2]+'\')'}"></div></td>
+        </tr>
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[3]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[4]+'\')'}"></div></td>
+        </tr>
+      </tbody>
+      <tbody v-if="disModeClass=='6'">
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[0]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[1]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[2]+'\')'}"></div></td>
+        </tr>
+        <tr>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[3]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[4]+'\')'}"></div></td>
+          <td><div :style="{backgroundImage:'url(\''+imgArr[5]+'\')'}"></div></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: '【未命名】'
+    },
+    count: {
+      type: Number,
+      default: 0
+    },
+    disMode: {
+      type: [Number, String],
+      default: 0
+    },
+    imgArr: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      disModeClass: ''
+    }
+  },
+  created () {
+    if (this.disMode === 2) {
+      this.disModeClass = '2-' + ((0).random() % 2 === 0 ? 'v' : 'h')
+    } else {
+      this.disModeClass = this.disMode.toString()
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.sww-album-title {
+  font-size: 0.8rem;
+  font-weight: bolder;
+  color:#333;
+  height: 1.5rem;
+  line-height: 1.5rem;
+  text-decoration: none;
+}
+.sww-album-count{
+  font-size: 0.6rem;
+  color:#777;
+  height: 1rem;
+  line-height: 1rem;
+}
+.sww-album-count span{
+  margin-right: 0.15rem;
+}
+.sww-album-tb{
+  --tb-height: 5rem;
+  margin-top: 0.5rem;
+  border-collapse:collapse;
+}
+.sww-album-tb div{
+  background-position: 50% 50%;
+  background-size: cover;
+  border: 0.05rem solid #ddd;
+  box-sizing: border-box;
+}
+.sww-album-tb.sww-album-tb-1 td div {
+  height: var(--tb-height);
+  width:  calc(var(--card-width) - 1rem);
+}
+.sww-album-tb.sww-album-tb-2-h td div{
+  height: calc(var(--tb-height) / 2);
+  width:  calc(var(--card-width) - 1rem);
+}
+.sww-album-tb.sww-album-tb-2-v td div{
+  height: calc(var(--tb-height));
+  width:  calc((var(--card-width) - 1rem - 0.1rem) / 2);
+}
+.sww-album-tb.sww-album-tb-2-v tr td:first-child div {
+  margin-right: 0.2rem;
+}
+.sww-album-tb.sww-album-tb-3 td[rowspan='2'] > div {
+  height: var(--tb-height);
+  width: calc(((var(--card-width) - 1rem) / 2) - 0.1rem);
+}
+.sww-album-tb.sww-album-tb-3 td:not([rowspan='2']) div {
+  height: calc(var(--tb-height) / 2);
+  width: calc(((var(--card-width) - 1rem) / 2) - 0.1rem);
+  margin-left: 0.2rem;
+}
+.sww-album-tb.sww-album-tb-4 td div{
+  height: calc((var(--tb-height)) / 2);
+  width:  calc((var(--card-width) - 1rem - 0.1rem) / 2);
+}
+.sww-album-tb.sww-album-tb-4 tr td:first-child div {
+  margin-right: 0.2rem;
+}
+.sww-album-tb.sww-album-tb-5 td[rowspan='2'] div {
+  height: calc(var(--tb-height));
+  width: calc((var(--card-width) - 1rem - 0.1rem) / 2);
+  margin-right: 0.2rem;
+}
+.sww-album-tb.sww-album-tb-5 td:not([rowspan='2']) div {
+  height: calc(var(--tb-height) / 2);
+  width: calc((var(--card-width) - 1rem - 0.1rem) / 4);
+}
+.sww-album-tb.sww-album-tb-6 td div {
+  height: calc(var(--tb-height) / 2);
+   width: calc((var(--card-width) - 1rem - 0.1rem) / 3);
+}
+</style>
--- src/page/album/album-list.vue
+++ src/page/album/album-list.vue
@@ -2,84 +2,86 @@
   <div class="x-main">
     <div></div>
     <ul class="sww-album-ul sww-clr-float">
-      <li>
-        <router-link class="sww-album-title" to="/album/123">活动图片</router-link>
-        <p class="sww-album-count"><span>1</span>张</p>
-        <table class="sww-album-tb sww-album-tb-1">
-          <tbody>
-            <tr>
-              <td><div style="background-image:url('../../../static/image/album/img_4.jpg');"></div></td>
-            </tr>
-          </tbody>
-        </table>
-      </li>
-      <li>
-        <p class="sww-album-title">活动图片</p>
-        <p class="sww-album-count"><span>2</span>张</p>
-        <table class="sww-album-tb sww-album-tb-2-h">
-          <tbody>
-            <tr>
-              <td><div style="background-image:url('../../../static/image/album/img_5.jpg');"></div></td>
-            </tr>
-            <tr>
-              <td><div style="background-image:url('../../../static/image/album/img_6.jpg');"></div></td>
-            </tr>
-          </tbody>
-        </table>
-      </li>
-      <li>
-        <p class="sww-album-title">活动图片</p>
-        <p class="sww-album-count"><span>2</span>张</p>
-        <table class="sww-album-tb sww-album-tb-2-v">
-          <tbody>
-            <tr>
-              <td><div style="background-image:url('../../../static/image/album/img_5.jpg');"></div></td>
-              <td><div style="background-image:url('../../../static/image/album/img_6.jpg');"></div></td>
-            </tr>
-          </tbody>
-        </table>
-      </li>
-      <li>
-        <p class="sww-album-title">技术图片</p>
-        <p class="sww-album-count"><span>12</span>张</p>
-        <table class="sww-album-tb sww-album-tb-3">
-          <tbody>
-            <tr>
-              <td rowspan="2"><div style="background-image:url('../../../static/image/album/img_1.jpg');"></div></td>
-              <td><div style="background-image:url('../../../static/image/album/img_2.jpg');"></div></td>
-            </tr>
-            <tr>
-              <td><div style="background-image:url('../../../static/image/album/img_3.jpg');"></div></td>
-            </tr>
-          </tbody>
-        </table>
-      </li>
-      <li>
-        <p class="sww-album-title">技术图片</p>
-        <p class="sww-album-count"><span>12</span>张</p>
-        <table class="sww-album-tb sww-album-tb-4">
-          <tbody>
-            <tr>
-              <td><div style="background-image:url('../../../static/image/album/img_7.jpg');"></div></td>
-              <td><div style="background-image:url('../../../static/image/album/img_8.jpg');"></div></td>
-            </tr>
-            <tr>
-              <td><div style="background-image:url('../../../static/image/album/img_9.jpg');"></div></td>
-              <td><div style="background-image:url('../../../static/image/album/img_10.jpg');"></div></td>
-            </tr>
-          </tbody>
-        </table>
+      <li v-for="(item,idx) in list" :key="idx">
+        <albumCard :title="item.title" :count="item.count" :disMode="item.disMode" :imgArr="item.imgArr"></albumCard>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import albumCard from '@/components/album/album-card'
 export default {
   name: 'HelloWorld',
-  components: {},
+  components: {albumCard},
   data () {
     return {
+      list: [
+        {
+          title: '算法图库',
+          count: 3,
+          disMode: 1,
+          imgArr: [
+            '../../../static/image/album/img_1.jpg',
+            '../../../static/image/album/img_9.jpg'
+          ]
+        },
+        {
+          title: 'CSS样式图片库',
+          count: 10,
+          disMode: 2,
+          imgArr: [
+            '../../../static/image/album/img_1.jpg',
+            '../../../static/image/album/img_9.jpg'
+          ]
+        },
+        {
+          title: '大屏素材图库',
+          count: 5,
+          disMode: 3,
+          imgArr: [
+            '../../../static/image/album/img_1.jpg',
+            '../../../static/image/album/img_9.jpg',
+            '../../../static/image/album/img_3.jpg'
+          ]
+        },
+        {
+          title: '大屏素材图库',
+          count: 5,
+          disMode: 4,
+          imgArr: [
+            '../../../static/image/album/img_1.jpg',
+            '../../../static/image/album/img_9.jpg',
+            '../../../static/image/album/img_3.jpg',
+            '../../../static/image/album/img_6.jpg'
+          ]
+        },
+        {
+          title: '大屏素材图库',
+          count: 5,
+          disMode: 5,
+          imgArr: [
+            '../../../static/image/album/img_1.jpg',
+            '../../../static/image/album/img_9.jpg',
+            '../../../static/image/album/img_3.jpg',
+            '../../../static/image/album/img_7.jpg',
+            '../../../static/image/album/img_5.jpg'
+          ]
+        },
+        {
+          title: '大屏素材图库',
+          count: 15,
+          disMode: 6,
+          imgArr: [
+            '../../../static/image/album/img_1.jpg',
+            '../../../static/image/album/img_9.jpg',
+            '../../../static/image/album/img_8.jpg',
+            '../../../static/image/album/img_4.jpg',
+            '../../../static/image/album/img_6.jpg',
+            '../../../static/image/album/img_7.jpg'
+          ]
+        }
+      ]
     }
   },
   created () {
@@ -103,64 +105,5 @@ ul.sww-album-ul > li {
   float: left;
   margin-right: .5rem;
   margin-bottom: .5rem;
-}
-ul.sww-album-ul > li .sww-album-title {
-  font-size: 0.8rem;
-  font-weight: bolder;
-  color:#333;
-  height: 1.5rem;
-  line-height: 1.5rem;
-  text-decoration: none;
-}
-ul.sww-album-ul > li .sww-album-count{
-  font-size: 0.6rem;
-  color:#777;
-  height: 1rem;
-  line-height: 1rem;
-}
-ul.sww-album-ul > li .sww-album-count span{
-  margin-right: 0.15rem;
-}
-.sww-album-tb{
-  --tb-height: 5rem;
-  margin-top: 0.5rem;
-  border-collapse:collapse;
-}
-.sww-album-tb div{
-  background-position: 50% 50%;
-  background-size: cover;
-  border: 0.05rem solid #ddd;
-  box-sizing: border-box;
-}
-.sww-album-tb.sww-album-tb-1 td div {
-  height: var(--tb-height);
-  width:  calc(var(--card-width) - 1rem);
-}
-.sww-album-tb.sww-album-tb-2-h td div{
-  height: calc(var(--tb-height) / 2);
-  width:  calc(var(--card-width) - 1rem);
-}
-.sww-album-tb.sww-album-tb-2-v td div{
-  height: calc(var(--tb-height));
-  width:  calc((var(--card-width) - 1rem - 0.1rem) / 2);
-}
-.sww-album-tb.sww-album-tb-2-v tr td:first-child div {
-  margin-right: 0.2rem;
-}
-.sww-album-tb.sww-album-tb-3 td[rowspan='2'] > div {
-  height: var(--tb-height);
-  width: calc(((var(--card-width) - 1rem) / 2) - 0.1rem);
-}
-.sww-album-tb.sww-album-tb-3 td:not([rowspan='2']) div {
-  height: calc(var(--tb-height) / 2);
-  width: calc(((var(--card-width) - 1rem) / 2) - 0.1rem);
-  margin-left: 0.2rem;
-}
-.sww-album-tb.sww-album-tb-4 td div{
-  height: calc((var(--tb-height)) / 2);
-  width:  calc((var(--card-width) - 1rem - 0.1rem) / 2);
-}
-.sww-album-tb.sww-album-tb-4 tr td:first-child div {
-  margin-right: 0.2rem;
 }
 </style>
