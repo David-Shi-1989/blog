79dafa10fa620f175e48758c8e099186df2de515
David-Shi-1989 <287841605@qq.com>
2018-11-03 09:41:50 +0800

    增加demolist

---
+++ src/components/demo/demo-card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="sww-demo-wrap">
+    <router-link class="sww-demo-title" :to="'/demo/'+id">{{title}}</router-link>
+    <p class="sww-demo-count"><span>{{count}}</span>张</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: '【未命名】'
+    },
+    count: {
+      type: Number,
+      default: 0
+    },
+    id: {
+      type: [String, Number],
+      default: '123'
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.sww-demo-title {
+  font-size: 0.8rem;
+  font-weight: bolder;
+  color:var(--color-linkable);
+  height: 1.5rem;
+  line-height: 1.5rem;
+  text-decoration: none;
+}
+.sww-demo-count{
+  font-size: 0.6rem;
+  color:var(--color-text-4);
+  height: 1rem;
+  line-height: 1rem;
+}
+.sww-demo-count span{
+  margin-right: 0.15rem;
+}
+</style>
--- src/page/demo/demo-list.vue
+++ src/page/demo/demo-list.vue
@@ -3,15 +3,27 @@
     <div class="sww-page-header sww-page-header-statistic">
       <span>18作品</span><span>520281行代码</span>
     </div>
-    <div class="sww-page-main"></div>
+    <div class="sww-page-main">
+      <ul class="sww-album-ul sww-clr-float">
+        <li v-for="(item,idx) in list" :key="idx">
+          <demoCard
+            :id="item.id"
+            :title="item.title"
+            :count="item.count"></demoCard>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import demoCard from '@/components/demo/demo-card'
 export default {
+  components: {demoCard},
   data () {
     return {
       list: [
+        {id: 'kops', title: '排序算法', count: 15, img: ''}
       ]
     }
   },
