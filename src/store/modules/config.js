da2c8e47cca8515aed6029f8d88b25b97d162d5a
David-Shi-1989 <287841605@qq.com>
2018-12-30 12:19:25 +0800

    将博客改为文章。

--- src/store/modules/config.js
+++ src/store/modules/config.js
@@ -1,7 +1,7 @@
 var state = {
   menus: [
     {title: '主页', to: '/', icon: 'fa fa-object-group', badge: 0},
-    {title: '博客', to: '/article', icon: 'fa fa-space-shuttle', badge: 8},
+    {title: '文章', to: '/article', icon: 'fa fa-space-shuttle', badge: 8},
     {title: '相册', to: '/album', icon: 'fa fa-thermometer', badge: 3},
     {title: '状态', to: '/post', icon: 'fa fa-tty', badge: 0},
     {title: '作品', to: '/demo', icon: 'fa fa-github', badge: 1}
