969af85732ceccf5928eb734858ff8a8913aaae4
David-Shi-1989 <287841605@qq.com>
2019-06-04 11:42:12 +0800

    demo页面修改。

--- src/components/demo/demo-card.vue
+++ src/components/demo/demo-card.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="sww-demo-wrap">
     <router-link class="sww-demo-title" :to="'/demo/'+id">{{title}}</router-link>
-    <p class="sww-demo-count"><span>{{count}}</span>张</p>
+    <br>
+    <p class="sww-star-wrap" :title="`${star}星`">
+      <i v-for="idx in 5"
+        :key="idx"
+        :data-idx="idx"
+        :class="star-idx + 1 <= 0 ? 'fa fa-star-o' : (star-idx+1<1?'fa fa-star-half-full':'fa fa-star')"
+      ></i>
+    </p>
+    <p class="sww-demo-description sww-text-ellipsis" :title="description">
+      {{description}} - {{star}}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
   props: {
     title: {
       type: String,
       default: '【未命名】'
     },
-    count: {
-      type: Number,
-      default: 0
+    description: {
+      type: String,
+      default: ''
     },
     id: {
       type: [String, Number],
       default: '123'
+    },
+    star: {
+      type: Number,
+      default: 0
     }
   },
   data () {
@@ -33,22 +46,37 @@ export default {
 }
 </script>
 
-<style scoped>
-.sww-demo-title {
-  font-size: 0.8rem;
-  font-weight: bolder;
-  color:var(--color-linkable);
-  height: 1.5rem;
-  line-height: 1.5rem;
-  text-decoration: none;
-}
-.sww-demo-count{
-  font-size: 0.6rem;
-  color:var(--color-text-4);
-  height: 1rem;
-  line-height: 1rem;
-}
-.sww-demo-count span{
-  margin-right: 0.15rem;
+<style scoped lang="less">
+.sww-demo-wrap {
+  display: inline-block;
+  width: 10rem;
+  border: 0.05rem solid #ddd;
+  padding: .2rem .4rem;
+  .sww-demo-title {
+    font-size: 0.8rem;
+    font-weight: bolder;
+    color:var(--color-linkable);
+    height: 1.5rem;
+    line-height: 1.5rem;
+    text-decoration: none;
+  }
+  .sww-star-wrap {
+    cursor: pointer;
+    display: inline-block;
+    i.fa {
+      font-size: 0.7rem;
+      &.fa-star, &.fa-star-half-full {
+        color: rgb(255,215,0);
+      }
+      &.fa-star-o {
+        color: #ccc;
+      }
+    }
+  }
+  .sww-demo-description{
+    font-size: 0.6rem;
+    height: 1.2rem;
+    color:var(--color-text-4);
+  }
 }
 </style>
--- src/page/demo/demo-list.vue
+++ src/page/demo/demo-list.vue
@@ -4,12 +4,13 @@
       <span>18作品</span><span>520281行代码</span>
     </div>
     <div class="sww-page-main">
-      <ul class="sww-album-ul sww-clr-float">
+      <ul class="sww-album-ul">
         <li v-for="(item,idx) in list" :key="idx">
           <demoCard
             :id="item.id"
             :title="item.title"
-            :count="item.count"></demoCard>
+            :description="item.description"
+            :star="item.star"></demoCard>
         </li>
       </ul>
     </div>
@@ -23,7 +24,13 @@ export default {
   data () {
     return {
       list: [
-        {id: 'kops', title: '排序算法', count: 15, img: ''}
+        {id: 'kops', title: '排序算法', description: '动态展示快排的过程和复杂度', img: '', star: 4.2},
+        {id: '12', title: '佣金计算', description: '利用Nodejs读取excel表格，进行计算，得到业务员的佣金结果', img: '', star: 3},
+        {id: 'kosdps', title: '其他', description: '15', img: '', star: 2.8},
+        {id: '123', title: '其他', description: '15', img: '', star: 5},
+        {id: 'kod12sdps', title: '其他', description: '15', img: '', star: 5.6},
+        {id: 'rrqew', title: '其他', description: '15', img: '', star: 0.6},
+        {id: 'kod12sdps', title: '其他', description: '15', img: '', star: 0}
       ]
     }
   },
@@ -32,5 +39,13 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.sww-album-ul {
+  list-style: none;
+  & >li {
+    display: inline-block;
+    margin-right: .5rem;
+    margin-bottom: .5rem;
+  }
+}
 </style>
